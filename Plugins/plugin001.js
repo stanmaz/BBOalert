ed74bbb41f66dc0d704ed4797dd5f1feccd173a2
stanmaz <stanmaz.git@gmail.com>
2022-06-21 22:16:12 +0200

    Update plugin001.js

--- Plugins/plugin001.js
+++ Plugins/plugin001.js
@@ -1,32 +1,9 @@
 (function () {
     var title = "Miscellaneous simple scripts";
     var cfg = {};
-    cfg.Enable_chat_timestamp = false;
     cfg.Move_table_left = false;
-    cfg.Remove_icons_from_tabs = false;
-    cfg.Large_bidding_box = false;
-    cfg.Modified_OK_button = false;
-    cfg.Swap_bidding_buttons = false;
-    cfg.Auto_chat_to_opponents = false;
-    cfg.Disable_alerts_with_casual_partner = false;
     addBBOalertEvent("onDataLoad", function () {
         addConfigBox(title, cfg);
-    });
-    addBBOalertEvent("onNewChatMessage", function () {
-        if (!cfg.Enable_chat_timestamp) return;
-        var ci = $("#chatDiv .chatOutputClass chat-list-item", window.parent.document).toArray();
-        var cs = ci[ci.length - 1].querySelector("span");
-        var now = new Date();
-        var hh = now.getHours().toString();
-        if (hh.length == 1) hh = '0' + hh;
-        var mn = now.getMinutes().toString();
-        if (mn.length == 1) mn = '0' + mn;
-        ci.forEach(function (cx) {
-            if (cx.textContent.match(/^[0-2][0-9][:][0-6][0-9]/) == null) {
-                var cs = cx.querySelector("span");
-                cs.textContent = hh + ':' + mn + ' ' + cs.textContent;
-            }
-        });
     });
     var moveTableLeftStyleText = `
     #navDiv .dealViewerToolbarClass {
@@ -51,18 +28,7 @@
             $("#move-table-left--style", window.parent.document).remove();
         }
     };
-    removeIconsFromTabs = function () {
-        if (cfg.Remove_icons_from_tabs) $(".verticalClass mat-icon", window.parent.document).hide();
-        if (!cfg.Remove_icons_from_tabs) $(".verticalClass mat-icon", window.parent.document).show();
-        $(".area-label", window.parent.document).css("font-weight", "bold");
-    };
-    addBBOalertEvent("onDataLoad", function () {
-        autoChatToOpponents();
-    });
-    autoChatToOpponents();
-
     addBBOalertEvent("onAnyMutation", function () {
         moveTableLeft(cfg.Move_table_left);
-        removeIconsFromTabs();
     });
 })();
