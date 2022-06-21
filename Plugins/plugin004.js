0b2a79dacdd1b52a2971ddf77a4b1196d2350329
stanmaz <stanmaz.git@gmail.com>
2022-06-21 22:27:28 +0200

    Create plugin004.js

---
+++ Plugins/plugin004.js
@@ -0,0 +1,35 @@
+(function () {
+    var title = "Miscellaneous simple scripts";
+    var cfg = {};
+    cfg.Move_table_left = false;
+    addBBOalertEvent("onDataLoad", function () {
+        addConfigBox(title, cfg);
+    });
+    var moveTableLeftStyleText = `
+    #navDiv .dealViewerToolbarClass {
+        left: 0px !important;
+    }    
+    #navDiv .coverClass {
+        left: coverclasspos !important;
+    }    
+    `;
+    var moveTableLeftStyle = document.createElement('style');
+    moveTableLeftStyle.id = 'move-table-left--style';
+    moveTableLeft = function (on) {
+        console.log("on = " + on);
+        if (on) {
+            var t = moveTableLeftStyleText.replace("coverclasspos", $("#navDiv .dealViewerToolbarClass", window.parent.document).width() + "px");
+            if (parent.document.head.querySelector("#move-table-left--style") == null) {
+                moveTableLeftStyle.innerHTML = t;
+                parent.document.head.appendChild(moveTableLeftStyle);
+            } else {
+                parent.document.head.querySelector("#move-table-left--style").innerHTML = t;
+            }
+        } else {
+            $("#move-table-left--style", window.parent.document).remove();
+        }
+    };
+    addBBOalertEvent("onAnyMutation", function () {
+        moveTableLeft(cfg.Move_table_left);
+    });
+})();
