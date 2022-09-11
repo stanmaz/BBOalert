cf0656e19840ac01b3bf794c7f36cba5dfd92cce
stanmaz <stanmaz.git@gmail.com>
2022-09-11 18:43:05 +0200

    Update stanmazPluginMV3.js

--- Plugins/stanmazPluginMV3.js
+++ Plugins/stanmazPluginMV3.js
@@ -242,8 +242,8 @@
     cfg.Modified_OK_button = false;
     cfg.Swap_bidding_buttons = false;
     cfg.Auto_chat_to_opponents = false;
-    cfg.Block_ads = false;
     cfg.Disable_alerts_with_casual_partner = false;
+    cfg.Block_ads = false;
     addBBOalertEvent("onDataLoad", function () {
         addConfigBox(title, cfg);
     });
