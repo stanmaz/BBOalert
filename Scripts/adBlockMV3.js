b3fc54d92c2cf7f425826221ea61b2dff4d2950c
stanmaz <stanmaz.git@gmail.com>
2022-09-12 22:32:35 +0200

    Create adBlockMV3.js

---
+++ Scripts/adBlockMV3.js
@@ -0,0 +1,8 @@
+bboalert
+Script,onAnyMutation
+$("#bbo_ad1",window.parent.document).hide();
+$("#bbo_ad2",window.parent.document).hide();
+$("#bbo_app",window.parent.document).css("left","0px");
+$("#bbo_app",window.parent.document).css("right","0px");
+$("#bbo_app",window.parent.document).css("width","");
+Script
