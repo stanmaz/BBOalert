bce53550c5391fd388c3a0b58784386e3cf32369
stanmaz <stanmaz.git@gmail.com>
2022-09-16 11:48:28 +0200

    Update CustomSyntaxScriptLibrary.js

--- Scripts/CustomSyntax/CustomSyntaxScriptLibrary.js
+++ Scripts/CustomSyntax/CustomSyntaxScriptLibrary.js
@@ -2,12 +2,12 @@
 //Script,onDataLoad,bidSymbolMap = new Map();
 //Script,onAuctionBegin,console.log("New map");bidSymbolMap = new Map();
 //Script,onNewAuction,console.log("Reset map");if (getContext() == '') bidSymbolMap = new Map();
-//Script,raise,R = bidSymbol("RAISE", B, getBidFromContext(2,0,5));
-//Script,jump_raise,R = bidSymbol("JUMP_RAISE", B, getBidFromContext(2,0,10));
-//Script,cuebid_rho,R = bidSymbol("CUEBID_RHO", B, getBidFromContext(3,0,5));
-//Script,cuebid_lho,R = bidSymbol("CUEBID_LHO", B, getBidFromContext(1,0,5));
+//Script,_raise_,R = bidSymbol("RAISE", B, getBidFromContext(2,0,5));
+//Script,_jump_raise_,R = bidSymbol("JUMP_RAISE", B, getBidFromContext(2,0,10));
+//Script,_cuebid_rho_,R = bidSymbol("CUEBID_RHO", B, getBidFromContext(3,0,5));
+//Script,_cuebid_lho_,R = bidSymbol("CUEBID_LHO", B, getBidFromContext(1,0,5));
 /*
-Script,shift
+Script,_shift_
 var bid = getBidFromContext(2,0,0);
 var R = "(";
 for (var i = 1; i < 5; i++) {
@@ -18,7 +18,7 @@ for (var i = 1; i < 5; i++) {
 R = R + ")";
 R = bidSymbol("SHIFT", B, R);
 Script
-Script,jump_shift
+Script,_jump_shift_
 var bid = getBidFromContext(2,0,5);
 var R = "(";
 for (var i = 1; i < 5; i++) {
@@ -31,4 +31,8 @@ R = bidSymbol("JUMP_SHIFT", B, R);
 Script
 Alias,RAISE,%_raise_%
 Alias,JUMP_RAISE,%_jump_raise_%
+Alias,SHIFT,%_shift_%
+Alias,JUMP_SHIFT,%_jump_shift_%
+Alias,CUE_RHO,%_cuebid_rho%
+Alias,CUE_LHO,%_cuebid_lho%
 */
