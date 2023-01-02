933b5683e14882fb656244c9ff027dc52a4323a8
stanmaz <stanmaz.git@gmail.com>
2023-01-02 11:18:38 +0100

    Update CustomSyntaxScriptLibrary.js

--- Scripts/CustomSyntax/CustomSyntaxScriptLibrary.js
+++ Scripts/CustomSyntax/CustomSyntaxScriptLibrary.js
@@ -1,8 +1,8 @@
 //Import,https://github.com/stanmaz/BBOalert/blob/master/Scripts/CustomSyntax/CustomSyntaxBase.js
-//Script,_raise_,R = bidSymbol("RAISE", B, getBidFromContext(2,0,5));
-//Script,_jump_raise_,R = bidSymbol("JUMP_RAISE", B, getBidFromContext(2,0,10));
-//Script,_cuebid_rho_,R = bidSymbol("CUEBID_RHO", B, getBidFromContext(3,0,5));
-//Script,_cuebid_lho_,R = bidSymbol("CUEBID_LHO", B, getBidFromContext(1,0,5));
+//Script,_raise_,R = bidSymbol("RAISE",C , B, getBidFromContext(2,0,5));
+//Script,_jump_raise_,R = bidSymbol("JUMP_RAISE",C , B, getBidFromContext(2,0,10));
+//Script,_cuebid_rho_,R = bidSymbol("CUEBID_RHO",C , B, getBidFromContext(3,0,5));
+//Script,_cuebid_lho_,R = bidSymbol("CUEBID_LHO",C , B, getBidFromContext(1,0,5));
 /*
 Script,_shift_
 var bid = getBidFromContext(2,0,0);
