ae394bd3c5cdc8c125654b5860d52ba029998628
stanmaz <stanmaz.git@gmail.com>
2023-01-04 11:28:45 +0100

    x

--- Systems/stanmaz/CustomSytax/dwururka_scripts.js
+++ Systems/stanmaz/CustomSytax/dwururka_scripts.js
@@ -1,1 +1,29 @@
-// BBOalert, Dwururka syntax scripts Version 1.0
+// BBOalert, Dwururka syntax scripts Version 1.0.100
+
+//Import,https://github.com/stanmaz/BBOalert/blob/master/Scripts/CustomSyntax/CustomSyntaxBase.js
+
+//Script,1CD
+if (matchContext("1[CD]", B)) {
+    R = bidSymbol("1CD","" , B, B);
+    bidSymbol("1CD_M1","", getCallbyIndex(B,2), getCallbyIndex(B,2));
+} else {
+    R = "??";
+}
+//Script
+//Script,1CD_M1,R = bidSymbolMap.get("1CD_M1");
+//Script,1HS
+if (matchContext("1[HS]", B)) {
+    R = bidSymbol("1HS","" , B, B);
+    bidSymbol("1HS_M1","", B, B);
+} else {
+    R = "??";
+}
+//Script
+//Script,1HS_M1,R = bidSymbolMap.get("1HS_M1");
+/*
+Shortcut,1CD,R=%1CD%
+Alias,1m,%1CD%
+Alias,1M,%1HS%
+,1m,4+card %1CD_M1% 
+,1M,4+card %1HS_M1% 
+*/
