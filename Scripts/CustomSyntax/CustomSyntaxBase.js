80c4a643657dbf58c44492f7c2a894d0321e0056
stanmaz <stanmaz.git@gmail.com>
2023-01-06 11:13:00 +0100

    Update CustomSyntaxBase.js

--- Scripts/CustomSyntax/CustomSyntaxBase.js
+++ Scripts/CustomSyntax/CustomSyntaxBase.js
@@ -1,4 +1,4 @@
 //BBOalert, custom syntax script library version 001
-//Script,onDataLoad,bidSymbolMap = new Map();
-//Script,onAuctionBegin,bidSymbolMap = new Map();
-//Script,onNewAuction,if (getContext() == '') bidSymbolMap = new Map();
+//Script,onDataLoad,bidSymbolMap.clear();
+//Script,onAuctionBegin,bidSymbolMap.clear();
+//Script,onNewAuction,if (getContext() == '') bidSymbolMap.clear();
