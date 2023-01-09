e47ea03ff5b69234b7e24068dbe4c53627a2f85c
stanmaz <stanmaz.git@gmail.com>
2023-01-09 14:50:37 +0100

    Update CustomSyntaxScriptLibrary.js

--- Scripts/CustomSyntax/CustomSyntaxScriptLibrary.js
+++ Scripts/CustomSyntax/CustomSyntaxScriptLibrary.js
@@ -1,12 +1,9 @@
 //BBOalert,Custom syntax library Version 1.3
-/*
-Import,https://github.com/stanmaz/BBOalert/blob/master/Scripts/CustomSyntax/CustomSyntaxBase.js
-*/
-//Script,_raise_,R = bidSymbol("RAISE",C , B, getBidFromContext(2,0,5));
-//Script,_jump_raise_,R = bidSymbol("JUMP_RAISE",C , B, getBidFromContext(2,0,10));
-//Script,_cuebid_rho_,R = bidSymbol("CUEBID_RHO",C , B, getBidFromContext(3,0,5));
-//Script,_cuebid_lho_,R = bidSymbol("CUEBID_LHO",C , B, getBidFromContext(1,0,5));
-//Script,_suit_bid_,R = "!" + B.slice(-1);
+//Script,_raise_,R = getBidFromContext(2,0,5);
+//Script,_jumpraise_,R = getBidFromContext(2,0,10);
+//Script,_cuebidrho_,R = getBidFromContext(3,0,5);
+//Script,_cuebidlho_,R = getBidFromContext(1,0,5);
+//Script,_suitbid_,R = "!" + B.slice(-1);
 //Script,_shift_
 var bid = getBidFromContext(2,0,0);
 var R = "(";
@@ -16,7 +13,6 @@ for (var i = 1; i < 5; i++) {
     if (i < 4) R = R + "|";
 }
 R = R + ")";
-R = bidSymbol("SHIFT",C , B, R);
 //Script
 //Script,_jump_shift_
 var bid = getBidFromContext(2,0,5);
@@ -27,18 +23,8 @@ for (var i = 1; i < 5; i++) {
     if (i < 4) R = R + "|";
 }
 R = R + ")";
-R = bidSymbol("JUMP_SHIFT",C , B, R);
 //Script
-Script,_opening_
-if (matchContext(C+B, B)) {
-    bidSymbol("opening_bid","" , B, B);
-} else {
-    R = "??";
-}
-//Script
-//Script,_step1_,R = bidSymbol("step1",C , B, getBidFromContext(2,-1,1));
-//Script,_step2_,R = bidSymbol("step1",C , B, getBidFromContext(2,-1,2));
-//Script,_step3_,R = bidSymbol("step1",C , B, getBidFromContext(2,-1,3));
-//Script,_step4_,R = bidSymbol("step1",C , B, getBidFromContext(2,-1,4));
-//Script,_opening_bid_,R = bidSymbolMap.get("opening_bid");
-//Script,_opening_suit_,R = bidSymbolMap.get("opening_bid").slice(-1);
+//Script,_step1_,R = getBidFromContext(2,-1,1);
+//Script,_step2_,R = getBidFromContext(2,-1,2);
+//Script,_step3_,R = getBidFromContext(2,-1,3);
+//Script,_step4_,R = getBidFromContext(2,-1,4);
