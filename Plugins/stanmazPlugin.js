1e506e220b0005eb0cc34a5cba6eb9506eea49bd
stanmaz <stanmaz.git@gmail.com>
2023-01-24 09:27:41 +0100

    x

--- Plugins/stanmazPlugin.js
+++ Plugins/stanmazPlugin.js
@@ -1,5 +1,5 @@
 
-//BBOalert,stanmazPlugin version 3.1
+//BBOalert,stanmazPlugin version 3.2
 
 function BBOcontext() {
     if (document.title != 'Bridge Base Online') return window.parent.document;
