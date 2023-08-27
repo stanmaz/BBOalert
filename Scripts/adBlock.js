bfa16f182099a2f344df587ea9f6838a979870a7
stanmaz <stanmaz.git@gmail.com>
2023-08-27 11:05:02 +0200

    Delete adBlock.js

--- Scripts/adBlock.js
+++
@@ -1,8 +0,0 @@
-bboalert
-Script,onAnyMutation
-$("#bbo_ad1").hide();
-$("#bbo_ad2").hide();
-$("#bbo_app").css("left","0px");
-$("#bbo_app").css("right","0px");
-$("#bbo_app").css("width","");
-Script
