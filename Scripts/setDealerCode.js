aadde43cf1fe774d4a5722f6bef252db85d0c3ca
stanmaz <stanmaz.git@gmail.com>
2023-10-04 15:11:25 +0200

    Update setDealerCode.js Italian UI added

--- Scripts/setDealerCode.js
+++ Scripts/setDealerCode.js
@@ -18,6 +18,7 @@ window.setDealerCode = function (dealerCode, dealer = "S", rotateDeals = true) {
                     $("menu-item div:contains('Source de la donne')", parent.window.document).trigger("click");
                     $("menu-item div:contains('Fuente de la mano')", parent.window.document).trigger("click");
                     $("menu-item div:contains('Quelle für Verteilungen')", parent.window.document).trigger("click");
+                    $("menu-item div:contains('Origine della mano')", parent.window.document).trigger("click");
                     break;
                 case 2:
                     // Select "Advanced" tab
@@ -25,6 +26,7 @@ window.setDealerCode = function (dealerCode, dealer = "S", rotateDeals = true) {
                     $("modal-content div:contains('Avancé')", parent.window.document).trigger("click");
                     $("modal-content div:contains('Avanzado')", parent.window.document).trigger("click");
                     $("modal-content div:contains('D:Fortgeschritten')", parent.window.document).trigger("click");
+                    $("modal-content div:contains('Avanzato')", parent.window.document).trigger("click");
                     break;
                 case 3:
                     // Select "Dealer" selector
