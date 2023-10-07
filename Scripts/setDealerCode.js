27b427114c6de02bf4d01d1c83b08e5dd4f2d743
stanmaz <stanmaz.git@gmail.com>
2023-10-07 08:00:17 +0200

    Update setDealerCode.js Dutch added

--- Scripts/setDealerCode.js
+++ Scripts/setDealerCode.js
@@ -19,6 +19,7 @@ window.setDealerCode = function (dealerCode, dealer = "S", rotateDeals = true) {
                     $("menu-item div:contains('Fuente de la mano')", parent.window.document).trigger("click");
                     $("menu-item div:contains('Quelle für Verteilungen')", parent.window.document).trigger("click");
                     $("menu-item div:contains('Origine della mano')", parent.window.document).trigger("click");
+                    $("menu-item div:contains('Spelbron')", parent.window.document).trigger("click");
                     break;
                 case 2:
                     // Select "Advanced" tab
@@ -27,6 +28,7 @@ window.setDealerCode = function (dealerCode, dealer = "S", rotateDeals = true) {
                     $("modal-content div:contains('Avanzado')", parent.window.document).trigger("click");
                     $("modal-content div:contains('D:Fortgeschritten')", parent.window.document).trigger("click");
                     $("modal-content div:contains('Avanzato')", parent.window.document).trigger("click");
+                    $("modal-content div:contains('Gevorderd')", parent.window.document).trigger("click");
                     break;
                 case 3:
                     // Select "Dealer" selector
