2dd5ccd1fc47c76b24abb2ab57277e19c055dfbf
stanmaz <stanmaz.git@gmail.com>
2024-03-11 15:55:39 +0100

    Add files via upload

--- Scripts/test/PlayWithBEN_bboalert.js
+++ Scripts/test/PlayWithBEN_bboalert.js
@@ -1,30 +1,29 @@
 //BBOalert, stanmaz new events 
 //BBOalert, version 20240311
-//Script,onAnyMutation
-console.log("onAnyMutation");
 //Script,onNewDeal
-console.log(getNow(true) + " onNewDeal");
+console.log(Date.now() + " onNewDeal");
 //Script,onMyCardsDisplayed
-console.log(getNow(true) + " onMyCardsDisplayed " + myCardsDisplayed);
+console.log(Date.now() + " onMyCardsDisplayed " + myCardsDisplayed);
 //Script,onNewAuction
-console.log(getNow(true) + " onNewAuction " + currentAuction);
+console.log(Date.now() + " onNewAuction " + currentAuction + " turn " + whosTurn() + " " + isMyTurn());
+
 //Script,onAuctionBegin
-console.log(getNow(true) + " onAuctionBegin" + " myTurn " + isMyTurn());
+console.log(Date.now() + " onAuctionBegin" + " myTurn " + isMyTurn());
 if (isMyTurn()) execUserScript('%onMyTurnToBid%');
 dummyDirection = "";
 declarerDirection = "";
 //Script,onAuctionEnd
-console.log(getNow(true) + " onAuctionEnd");
+console.log(Date.now() + " onAuctionEnd");
 //Script,onBiddingBoxDisplayed
-console.log(getNow(true) + " onBiddingBoxDisplayed");
+console.log(Date.now() + " onBiddingBoxDisplayed");
 //Script,onAuctionBoxDisplayed
-console.log(getNow(true) + " onAuctionBoxDisplayed");
+console.log(Date.now() + " onAuctionBoxDisplayed");
 //Script,onMyLead
-console.log(getNow(true) + " onMyLead");
+console.log(Date.now() + " onMyLead");
 //Script,onDealEnd
-console.log(getNow(true) + " onDealEnd");
+console.log(Date.now() + " onDealEnd");
 //Script,onNewPlayedCard
-console.log(getNow(true) + " onNewPlayedCard " + getPlayedCards() + " turn " + whosTurn());
+console.log(Date.now() + " onNewPlayedCard " + getPlayedCards() + " turn " + whosTurn());
 if (whosTurn() != "") {
     execUserScript('%onBeforePlayingCard%');
 }
@@ -34,25 +33,24 @@ if (whosTurn() == myDirection()) who = "me";
 if (whosTurn() == directionLHO()) who = "lho";
 if (whosTurn() == partnerDirection()) who = "partner";
 if (whosTurn() == directionRHO()) who = "rho";
-console.log(getNow(true) + " onBeforePlayingCard " + whosTurn() + " " + who);
-if (isMyTurn()) execUserScript('%onMyTurnToPlay%');
+console.log(Date.now() + " onBeforePlayingCard " + whosTurn() + " " + who);
 if ((who == "partner" && partnerDirection() == dummyDirection) || (isMyTurn())) execUserScript('%onMyTurnToPlay%');
 //Script,onNewActivePlayer
-console.log(getNow(true) + " onNewActivePlayer " + activePlayer + " turn " + whosTurn() + " " + isMyTurn());
+console.log(Date.now() + " onNewActivePlayer " + activePlayer);
 if (auctionBoxDisplayed) {
-    var ctx = getContext();
+    let ctx = getContext();
     if ((ctx.length >= 8) && (ctx.endsWith('------'))) {
-        dummyDirection = "NESWNESW".charAt("NESW".indexOf(whosTurn()) + 1);
-        declarerDirection = "NESWNESW".charAt("NESW".indexOf(whosTurn()) + 3);
+        dummyDirection = "NESWNESW".charAt("NESW".indexOf(whosTurn())+1);
+        declarerDirection = "NESWNESW".charAt("NESW".indexOf(whosTurn())+3);
         execUserScript('%onBeforePlayingCard%');
     } else {
         if (isMyTurn()) execUserScript('%onMyTurnToBid%');
     }
 }
 //Script,onMyTurnToBid
-console.log(getNow(true) + " onMyTurnToBid");
+console.log(Date.now() + " onMyTurnToBid context: "+ getContext());
 //Script,onMyTurnToPlay
-console.log(getNow(true) + " onMyTurnToPlay");
+console.log(Date.now() + " onMyTurnToPlay Cards played: " + getPlayedCards());
 //Script
 
 //BBOalert,myFunctions
@@ -60,8 +58,8 @@ console.log(getNow(true) + " onMyTurnToPlay");
 dummyDirection = "";
 declarerDirection = "";
 getCardByValue = function (cv) {
-    var card = $("bridge-screen", parent.window.document).find(".topLeft:visible").filter(function () {
-        if (replaceSuitSymbols(this.textContent, "") == cv) return this;
+    var card =  $("bridge-screen" ,parent.window.document).find(".topLeft:visible").filter(function () {
+        if (replaceSuitSymbols(this.textContent,"") == cv) return this;
     });
     if (card.length != 0) return card[0];
     return null;
@@ -77,7 +75,7 @@ playCardByValue = function (cv) {
 
 getDummyCards = function () {
     playedCards = [];
-    var cards = $("bridge-screen", parent.window.document).find(".cardClass:visible").each(function () {
+    var cards =  $("bridge-screen" ,parent.window.document).find(".cardClass:visible").each(function () {
         if (this.style.zIndex.startsWith("3")) {
             var c = $(this).find(".topLeft").text();
             playedCards.push(replaceSuitSymbols(c, ""));
@@ -88,7 +86,7 @@ getDummyCards = function () {
 
 getMyCards = function () {
     playedCards = [];
-    var cards = $("bridge-screen", parent.window.document).find(".cardClass:visible").each(function () {
+    var cards =  $("bridge-screen" ,parent.window.document).find(".cardClass:visible").each(function () {
         if (this.style.zIndex.startsWith("1")) {
             var c = $(this).find(".topLeft").text();
             playedCards.push(replaceSuitSymbols(c, ""));
@@ -98,43 +96,43 @@ getMyCards = function () {
 }
 
 function getCard(index) {
-    var card = parent.$(".cardClass:visible").filter(function () {
-        return $(this).css('z-index') == index;
-    }).text();
-    if (card.length == 6) {
-        card = "T" + card.slice(-1);
-    } else card = card.slice(0, 2);
-    return card;
+	var card = parent.$(".cardClass:visible").filter(function () {
+		return $(this).css('z-index') == index;
+	}).text();
+	if (card.length == 6) {
+		card = "T" + card.slice(-1);
+	} else card = card.slice(0, 2);
+	return card;
 }
 
 isMyTurn = function () {
-    if ($("bridge-screen", parent.window.document).find(".nameBarClass:visible").first().css("background-color") == 'rgb(255, 206, 0)') {
+    if ($("bridge-screen" ,parent.window.document).find(".nameBarClass:visible").first().css("background-color") == 'rgb(255, 206, 0)') {
         return (activePlayer.substring(1).toLowerCase() == whoAmI().toLowerCase());
     }
     return false;
 }
 
 whosTurn = function () {
-    return $("bridge-screen,parent", parent.window.document).find(".nameBarClass:visible").filter(function () {
+    return $("bridge-screen,parent",parent.window.document).find(".nameBarClass:visible").filter(function() {
         if (this.style.backgroundColor == 'rgb(255, 206, 0)') return this;
     }).find(".directionClass").text();
 }
 
 delayedAlert = function (txt, delay = 0) {
-    setTimeout(function () {
+    setTimeout(function() {
         alert(txt);
     }, delay)
 }
 
 
 window.getCard = function (index) {
-    var card = parent.$(".cardClass:visible").filter(function () {
-        return ($(this).css('z-index') == index);
-    }).text();
-    if (card.length == 6) {
-        card = "T" + card.slice(-1);
-    } else card = card.slice(0, 2);
-    return card;
+	var card = parent.$(".cardClass:visible").filter(function () {
+		return ($(this).css('z-index') == index);
+	}).text();
+	if (card.length == 6) {
+		card = "T" + card.slice(-1);
+	} else card = card.slice(0, 2);
+	return card;
 }
 
 //Script
