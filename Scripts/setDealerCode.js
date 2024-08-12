accb3ea1d194c11195a36fe66b53654154073cf6
stanmaz <stanmaz.git@gmail.com>
2024-08-12 16:38:51 +0200

    Update setDealerCode.js

--- Scripts/setDealerCode.js
+++ Scripts/setDealerCode.js
@@ -55,7 +55,7 @@ window.setDealerCode = function (dealerCode, dealer = "S", rotateDeals = true) {
                     $("modal-content div:contains('Напреднал')", parent.window.document).trigger("click");
                     $("modal-content div:contains('İleri düzey')", parent.window.document).trigger("click");
                     $("modal-content div:contains('高級')", parent.window.document).trigger("click"); //lang=zh-TW
-//                    $("modal-content div:contains('高级')", parent.window.document).trigger("click"); //lang=zh-CN is identical to sh-TW
+                    $("modal-content div:contains('高级')", parent.window.document).trigger("click"); //lang=zh-CN
                     if (dealerCode == "") clearInterval(intrv);
                     break;
                 case 3:
