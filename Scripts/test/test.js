07be4be646587a1f586cdd85bc2e81ba121b66be
stanmaz <stanmaz.git@gmail.com>
2025-05-11 08:37:35 +0200

    Update test.js

--- Scripts/test/test.js
+++ Scripts/test/test.js
@@ -2,4 +2,5 @@
 url = "https://github.com/stanmaz/BBOalert/blob/master/Systems/TrefleRouge/System/RCbase.tex";
 fetchWebData(url, function() {
   console.log("fetchWebData ", data.length);
+  success(data);
 }, failure);
