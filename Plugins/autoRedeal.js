e7471caa4e9445d487e0f0bf99d45c7957c25930
stanmaz <stanmaz.git@gmail.com>
2025-10-30 07:53:27 +0100

    Add files via upload

---
+++ Plugins/autoRedeal.js
@@ -0,0 +1,18 @@
+(function () {
+	var title = "Auto_redeal";
+	var cfg = {};
+	cfg.Enable_redeal = false;
+	cfg.max_deals = 64;
+	addBBOalertEvent("onDataLoad", function () {
+		if (addConfigBox(title, cfg) != null) {
+			addBBOalertEvent("onNewAuction", function () {
+				if (!cfg.Enable_redeal) return;
+				if (getDealNumber() < cfg.max_deals) {
+					var ctx = getContext();
+					if ((ctx.length > 6) && ctx.endsWith("------")) $(".redeal-button", PWD).click();
+				}
+			});
+		}
+	});
+})();
+
