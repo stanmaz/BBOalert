f4fe5edcd456a21895a3439e030b240a116ed137
stanmaz <stanmaz.git@gmail.com>
2025-10-31 21:32:42 +0100

    Add files via upload

---
+++ Plugins/autoRedeal_beta.js
@@ -0,0 +1,23 @@
+(function () {
+	var title = "Auto redeal at auction end";
+	var cfg = {};
+	cfg.Enable_redeal = false;
+	cfg.max_deals = 64;
+	addBBOalertEvent("onDataLoad", function () {
+		if (addConfigBox(title, cfg) != null) {
+			addBBOalertEvent("onNewAuction", function () {
+				if (!cfg.Enable_redeal) return;
+				if (cfg.max_deals < 1) return
+				var ctx = getContext();
+				if (ctx.length < 8) return;
+				if (!ctx.endsWith("------")) return;
+				setChatDestination("Table");
+				var msg = `autoRedeal,${getContext()},${getHandBySeat('N')},${getHandBySeat('E')},${getHandBySeat('S')},${getHandBySeat('W')}`;
+				msg = replaceSuitSymbols(msg, "") + "\\n";
+				setChatMessage(msg, true);
+				cfg.max_deals--;
+				$(".redeal-button", PWD).click();
+			})
+		}
+	});
+})();
