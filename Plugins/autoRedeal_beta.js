426063a39fe1bfccf39d50b55afc055a53cb24dd
stanmaz <stanmaz.git@gmail.com>
2025-11-02 16:45:25 +0100

    Add confirmation for clipboard data copy on redeal

--- Plugins/autoRedeal_beta.js
+++ Plugins/autoRedeal_beta.js
@@ -12,18 +12,34 @@
 	var cfg = {};
 	cfg.Enable_redeal = false;
 	cfg.max_deals = 64;
+	var txt = "";
 	addBBOalertEvent("onDataLoad", function () {
 		if (addConfigBox(title, cfg) != null) {
 			addBBOalertEvent("onNewAuction", function () {
 				if (!cfg.Enable_redeal) return;
-				if (cfg.max_deals < 1) return
+				if (cfg.max_deals < 1) {
+					if confirm("Press OK to copy data to the clipboard") {
+						writeToClipboard(txt);
+						return;
+					}
+				}
 				var ctx = getContext();
 				if (ctx.length < 8) return;
 				if (!ctx.endsWith("------")) return;
+				var dealer = "NESW".charAt((getDealNumber() - 1) % 4);
+				var vul = areWeVulnerable()+areTheyVulnerable();
+				if (vul == "@n@N") vul = "none";
+				if (vul == "@v@N") vul = "NS";
+				if (vul == "@n@V") vul = "EW";
+				if (vul == "@v@V") vul = "All";
 				setChatDestination("Table");
-				var msg = `autoRedeal,${getContext()},[Board "${getDealNumber()}"]
+				var msg = `${getContext()},[Event "autoRedeal"]
+				[Board "${getDealNumber()}"]
+				[Dealer "${dealer}"]
+				[Vulnerable "${vul}"]
 				[Deal "N:${hand2PBN(getHandBySeat('N'))} ${hand2PBN(getHandBySeat('E'))} ${hand2PBN(getHandBySeat('S'))} ${hand2PBN(getHandBySeat('W'))}]`;
 				msg = replaceSuitSymbols(msg, "");
+				txt = txt+msg;
 				console.log(msg);
 //				setChatMessage(msg + "\\n", true);
 				cfg.max_deals--;
