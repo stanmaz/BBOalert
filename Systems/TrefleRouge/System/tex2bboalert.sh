185626e84610af12d5d04db7cf52ba4428a61ce9
stanmaz <stanmaz.git@gmail.com>
2025-11-15 10:50:36 +0100

    Add files via upload

--- Systems/TrefleRouge/System/tex2bboalert.sh
+++ Systems/TrefleRouge/System/tex2bboalert.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
-echo BBOalert, Trefle Rouge > RC.txt
-node tex2bboalert.js RCbase.tex >> RC.txt
-node tex2bboalert.js RC1T.tex >> RC.txt
-node tex2bboalert.js RC1K.tex >> RC.txt
-node tex2bboalert.js RC1C.tex >> RC.txt
-node tex2bboalert.js RC1P.tex >> RC.txt
-node tex2bboalert.js RC1SA.tex >> RC.txt
-node tex2bboalert.js RC2m.tex >> RC.txt
-node tex2bboalert.js RC2C.tex >> RC.txt
+echo BBOalert, Trefle Rouge > RC.bbo
+node tex2bboalert.js RCbase.tex >> RC.bbo
+node tex2bboalert.js RC1T.tex >> RC.bbo
+node tex2bboalert.js RC1K.tex >> RC.bbo
+node tex2bboalert.js RC1C.tex >> RC.bbo
+node tex2bboalert.js RC1P.tex >> RC.bbo
+node tex2bboalert.js RC1SA.tex >> RC.bbo
+node tex2bboalert.js RC2m.tex >> RC.bbo
+node tex2bboalert.js RC2C.tex >> RC.bbo
 
 
