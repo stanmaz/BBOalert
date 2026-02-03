99c2c1c5a264351b62b67568975d226447967aed
stanmaz <stanmaz.git@gmail.com>
2026-02-03 18:01:23 +0100

    Add files via upload

--- Plugins/AutoPass.js
+++ Plugins/AutoPass.js
@@ -1,18 +1,30 @@
 (function () {
 	console.log("AutoPass version 1.0");
 
+	function clickPass() {
+		try {
+		getBiddingBoxButtons()[12].click();
+		setTimeout(() => {
+			getBiddingBoxButtons()[16].click();
+		}, 100);
+		getBiddingBoxButtons()[16].click();
+		} catch {}
+	}
+
 	var title = "Automatic pass on teaching table";
 	var cfg = {};
 	cfg.West = false;
 	cfg.North = false;
 	cfg.East = false;
 	cfg.South = false;
-	var EVENT_LOG = localStorage.getItem('PBNcapture');
-	if (EVENT_LOG == null) EVENT_LOG = '';
 	addBBOalertEvent("onDataLoad", function () {
 		if (addConfigBox(title, cfg) != null) {
 			addBBOalertEvent("onNewActivePlayer", function () {
-				}
+				var activePlayer = getActivePlayer();
+				if (cfg.West && (activePlayer.charAt(0) == "W")) clickPass();
+				if (cfg.North && (activePlayer.charAt(0) == "N")) clickPass();
+				if (cfg.East && (activePlayer.charAt(0) == "E")) clickPass();
+				if (cfg.South && (activePlayer.charAt(0) == "S")) clickPass();
 			});
 		}
 	});
